87bbf208cd366c6a3dbee11d708fcc8ee7087741
Edward Smith <edwardmsmith@gmail.com>
2013-06-27 10:45:04 -0500

    Place cursor at end of comment (when turning a selection into a comment) so the user can keep typing more comment.
    Thought process is, I'm typing, oh, I wanted this to be a comment, highlight what
    I've already typed, hit comment button, keep typing.

--- src/commands/comment.js
+++ src/commands/comment.js
@@ -13,22 +13,13 @@
             len         = container.length,
             elementToSetCaretAfter,
             whitespace;
-
-
-        /*  This chunk of code works if we've selected a bunch of text
-            and are making it a comment.  But, it doesn't work if we click
-            the button and start typing, then click the button again.
-            
-            If we're click-button-type-comment-click-button, what's the 
-
-        */
         if (range.collapsed && range.endOffset === len) {
-          //wysihtml5.commands.formatInline.exec(composer, command, NODE, CLASS, REGEXP);
-          // The range is collapsed (nothing selected), and the cursor is at the
-          //  end of the text node.  We want to move the cursor AFTER the comment_tag
-          // If the comment tag does not have a following sibling, its probably
-          //   the last item in the body.  If so, we should add an "invisible"
-          //   text node after it.
+          /* The range is collapsed (nothing selected), and the cursor is at the
+                end of the text node.  We want to move the cursor AFTER the comment_tag
+             If the comment tag does not have a following sibling, its probably
+                the last item in the body.  If so, we should add an "invisible"
+                text node after it.
+          */
           elementToSetCaretAfter = comment_tag;
           if (!comment_tag.nextElementSibling) {
             composer.selection.setAfter(comment_tag);
@@ -44,17 +35,14 @@
           comment_tag.parentNode.replaceChild(text_node, comment_tag);
           composer.selection.selectNode(text_node);
         }
-
       } else {
         var inner_span      = composer.doc.createElement("span"),
             toggle          = composer.doc.createElement("span"),
             doc             = composer.doc,
             tempClass       = "_wysihtml5-temp-" + (+new Date()),
             tempClassRegExp = /non-matching-class/g,
-            i               = 0,
-            length,
-            text_span,
-            text_spans    ;
+            old_range,
+            new_range;
         // Create the toggle
         toggle.setAttribute("class","inline-comment-control");
         toggle.setAttribute("contenteditable","false");
@@ -64,17 +52,14 @@
         inner_span.setAttribute("tabindex","1");
         // Wrap the inner comment span with another span
         wysihtml5.commands.formatInline.exec(composer, command, NODE, CLASS, REGEXP);
-        // wysihtml5.commands.formatInline.exec(composer, command, NODE, tempClass, tempClassRegExp);
-        // text_spans = doc.querySelectorAll(NODE + "." + tempClass);
-        // length = text_spans.length;
-        // for (; i<length; i++) {
-        //   text_span = text_spans[i];
-        //   text_span.removeAttribute("class");
-        //   text_span.setAttribute("class",CLASS);
-        //   // text_span.setAttribute("contenteditable","false");
-        // }
         composer.selection.surround(inner_span);
-        wysihtml5.dom.insert(toggle).after(inner_span);        
+        wysihtml5.dom.insert(toggle).after(inner_span);
+        // We want to set the cursor/selection 
+        composer.selection.selectNode(inner_span);
+        old_range = composer.selection.getRange();
+        new_range = old_range.cloneRange();
+        new_range.collapse(false);
+        composer.selection.setSelection(new_range);
       }
     },
 
